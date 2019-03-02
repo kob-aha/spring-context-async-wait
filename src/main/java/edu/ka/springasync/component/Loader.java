a8e314ccdaa4f5fc6ab1853987809ebc8871e7f6
Koby Aharon <kobyahron@gmail.com>
2019-03-02 22:06:43 +0200

    Add wait using Semaphore

--- src/main/java/edu/ka/springasync/component/Loader.java
+++ src/main/java/edu/ka/springasync/component/Loader.java
@@ -1,11 +1,11 @@
 package edu.ka.springasync.component;
 
-import edu.ka.springasync.component.impl.ParentComponent;
 import edu.ka.springasync.event.ComponentLoadedEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -14,15 +14,19 @@ import java.util.concurrent.locks.ReentrantLock;
 @Component
 public class Loader {
 
+    public static final int WAIT_TIMEOUT_SECONDES = 20;
     @Autowired
     private AppComponent parentComponent;
 
     private Lock loadWaitLock;
     private Condition componentLoadedCondition;
 
+    private Semaphore loadWaitSemaphore;
+
     public Loader() {
         loadWaitLock = new ReentrantLock();
         componentLoadedCondition = loadWaitLock.newCondition();
+        loadWaitSemaphore = new Semaphore(0);
     }
 
     public void loadApp() {
@@ -38,11 +42,23 @@ public class Loader {
         }
     }
 
-    private boolean waitUntilLoaded() {
+    private void sleep() {
+        System.out.println("Start sleeping in Loader");
+
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(30));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Finished sleeping in Loader");
+    }
+
+    private boolean waitUntilLoadedCondition() {
         boolean retVal = false;
         try {
             loadWaitLock.lock();
-            retVal = componentLoadedCondition.await(5, TimeUnit.SECONDS);
+            retVal = componentLoadedCondition.await(WAIT_TIMEOUT_SECONDES, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
@@ -52,14 +68,33 @@ public class Loader {
         return retVal;
     }
 
+    private boolean waitUntilLoadedSemaphore() {
+        boolean retVal = false;
+        try {
+            retVal = loadWaitSemaphore.tryAcquire(WAIT_TIMEOUT_SECONDES, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return retVal;
+    }
+
     @EventListener
     public void onEvent(ComponentLoadedEvent event) {
+//        notifyUsingCondition();
+        notifyUsingSemaphore();
+    }
+
+    private void notifyUsingCondition() {
         try {
             loadWaitLock.lock();
             componentLoadedCondition.signal();
         } finally {
             loadWaitLock.unlock();
         }
+    }
 
+    private void notifyUsingSemaphore() {
+        loadWaitSemaphore.release();
     }
 }
