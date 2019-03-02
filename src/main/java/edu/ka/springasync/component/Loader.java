bc75c125c589004c41b6239769ee2da29238360d
Koby Aharon <kobyahron@gmail.com>
2019-03-02 21:45:11 +0200

    Making Loader synchronous by using Condition to wait for parent component to finish loading

--- src/main/java/edu/ka/springasync/component/Loader.java
+++ src/main/java/edu/ka/springasync/component/Loader.java
@@ -6,19 +6,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 @Component
 public class Loader {
 
     @Autowired
     private AppComponent parentComponent;
 
+    private Lock loadWaitLock;
+    private Condition componentLoadedCondition;
+
+    public Loader() {
+        loadWaitLock = new ReentrantLock();
+        componentLoadedCondition = loadWaitLock.newCondition();
+    }
+
     public void loadApp() {
         System.out.println("Start loading parent component");
         parentComponent.loadComponent();
+
+        boolean parentFinishedLoading = waitUntilLoaded();
+
+        if (parentFinishedLoading) {
+            System.out.println("Parent finished loading");
+        } else {
+            System.out.println(" ********** ERROR: Parent didn't finish loading within the time limit ********** ");
+        }
+    }
+
+    private boolean waitUntilLoaded() {
+        boolean retVal = false;
+        try {
+            loadWaitLock.lock();
+            retVal = componentLoadedCondition.await(5, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            loadWaitLock.unlock();
+        }
+
+        return retVal;
     }
 
     @EventListener
     public void onEvent(ComponentLoadedEvent event) {
-        System.out.println("Got event loaded. Parent finished loading");
+        try {
+            loadWaitLock.lock();
+            componentLoadedCondition.signal();
+        } finally {
+            loadWaitLock.unlock();
+        }
+
     }
 }
